7e1535eb502152e2c1c6dd4930ac449b12bad087
Felix Flores <ffloresflores@csumb.edu>
2023-11-15 20:01:15 -0800

    Adding Classes

---
+++ src/src/main/java/com/sportstracker/SportstrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sportstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportstrackerApplication {  
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportstrackerApplication.class, args);
+	}
+
+}
---
+++ src/src/main/java/com/sportstracker/config/CorsConfig.java
@@ -0,0 +1,24 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // Add allowed origins, methods, headers, etc.
+        config.addAllowedOrigin("http://localhost:3000"); // Add your frontend URL
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        //other CORS configurations as needed
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
